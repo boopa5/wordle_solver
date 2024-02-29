be231dc2ccb6270e03ea9e4029af5ad6b958ec4c
Ben Asperheim <basperheim1@gmail.com>
2024-02-28 22:45:19 -0800

    adding everything

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from wordle import *
 from wordle_bot import *
-
+                    
 with open("word_list.txt") as f:
     word_list = [line.rstrip() for line in f]
 
-play_wordle_computer(word_list, 'house', 6)
+play_wordle_computer(word_list, 'devil', 6)
